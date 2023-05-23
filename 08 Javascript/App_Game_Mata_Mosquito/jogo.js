2c71516ba86d2804b43b5e304739a8b7b97aa8ae
Daniel dos Santos <theffice@gmail.com>
2023-05-22 21:10:46 -0300

    Aula 222
    Criando posições randômicas

--- 08 Javascript/App_Game_Mata_Mosquito/jogo.js
+++ 08 Javascript/App_Game_Mata_Mosquito/jogo.js
@@ -9,3 +9,23 @@ function ajustaTamanhoPalcoJogo() { // Definindo a dimensão do palco do jogo
 }
 
 ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica() {
+	
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
+	
+	//lógica ternária
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
+	
+	//criar elemento html
+	var mosquito = document.createElement('img')
+	mosquito.src = "imagens/mosquito.png"
+	mosquito.className = 'mosquito1'
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+	
+	document.body.appendChild(mosquito)
+}
