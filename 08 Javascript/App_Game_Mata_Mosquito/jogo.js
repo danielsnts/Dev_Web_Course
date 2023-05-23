9b3b6cf651b6d51d1b3ee4be67f6a2654b33649f
Daniel dos Santos <theffice@gmail.com>
2023-05-23 18:41:24 -0300

    Aula 223
    Tamanhos randômicos

--- 08 Javascript/App_Game_Mata_Mosquito/jogo.js
+++ 08 Javascript/App_Game_Mata_Mosquito/jogo.js
@@ -10,7 +10,7 @@ function ajustaTamanhoPalcoJogo() { // Definindo a dimensão do palco do jogo
 
 ajustaTamanhoPalcoJogo()
 
-function posicaoRandomica() {
+function posicaoRandomica() { //Fornece uma posição aleatória
 	
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
@@ -22,10 +22,25 @@ function posicaoRandomica() {
 	//criar elemento html
 	var mosquito = document.createElement('img')
 	mosquito.src = "imagens/mosquito.png"
-	mosquito.className = 'mosquito1'
+	mosquito.className = tamanhoAleatorio()
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
 	
 	document.body.appendChild(mosquito)
+}
+
+function tamanhoAleatorio() {
+	var classe = Math.floor(Math.random() * 3)
+	
+	switch(classe) {
+		case 0:
+			return 'mosquito1'
+			
+		case 1:
+			return 'mosquito2'
+			
+		case 2:
+			return 'mosquito3'
+	}
 }
