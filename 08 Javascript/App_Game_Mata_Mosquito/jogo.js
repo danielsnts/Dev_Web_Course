59e5933fa9083d723a2f536c0a7195d30e0905db
Daniel dos Santos <theffice@gmail.com>
2023-05-24 20:57:45 -0300

    Aula 225
    Criando e removendo mosquitos a cada ciclo de tempo

--- 08 Javascript/App_Game_Mata_Mosquito/jogo.js
+++ 08 Javascript/App_Game_Mata_Mosquito/jogo.js
@@ -1,3 +1,4 @@
+
 var altura = 0
 var largura = 0
 
@@ -10,22 +11,31 @@ function ajustaTamanhoPalcoJogo() { // Definindo a dimensão do palco do jogo
 
 ajustaTamanhoPalcoJogo()
 
+
 function posicaoRandomica() { //Fornece uma posição aleatória
 	
+	
+	//remover o mosquito anterior (caso exista)
+	if(document.getElementById('mosquito')) {
+		document.getElementById('mosquito').remove()
+	}
+
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
-	
 	//lógica ternária
 	posicaoX = posicaoX < 0 ? 0 : posicaoX
 	posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+	console.log(posicaoX, posicaoY)
 	
 	//criar elemento html
 	var mosquito = document.createElement('img')
-	mosquito.src = "imagens/mosquito.png"
-	mosquito.className = tamanhoAleatorio() + ladoAleatorio() //A soma de duas strings equivale a duas classes nesse caso
+	mosquito.src = 'imagens/mosquito.png'
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio() //A soma de duas strings equivale a duas classes nesse caso
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
+	mosquito.id = 'mosquito'
 	
 	document.body.appendChild(mosquito)
 }
@@ -45,7 +55,7 @@ function tamanhoAleatorio() {
 	}
 }
 
-function ladoAleatorio() {
+function ladoAleatorio() {  //Retorna um lado aleatório
 	var classe = Math.floor(Math.random() * 2)
 	
 	switch(classe) {
